6a0003ceed92df8e796a4cedd79cc4e785d47cdc
Jeevan <shresthaj1986@gmail.com>
2024-11-18 19:39:21 +0545

    minor changes

--- util/auth.js
+++ util/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "AIzaSyAy7D47ZX2bGtJNgUiQGU2rtpnE-IX4yUs";
+const API_KEY = "";
 
 async function authenticate(mode, email, password) {
   const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
@@ -16,15 +16,7 @@ async function authenticate(mode, email, password) {
   return token;
 }
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyAy7D47ZX2bGtJNgUiQGU2rtpnE-IX4yUs",
-  authDomain: "e-commerce-3b6f5.firebaseapp.com",
-  projectId: "e-commerce-3b6f5",
-  storageBucket: "e-commerce-3b6f5.appspot.com",
-  messagingSenderId: "1014480048764",
-  appId: "1:1014480048764:web:7b5b9b9b1c7b7d6e6d9b41",
-  measurementId: "G-8T0QWZQJQ8",
-};
+export const firebaseConfig = {};
 
 export function createUser(email, password) {
   return authenticate("signUp", email, password);
